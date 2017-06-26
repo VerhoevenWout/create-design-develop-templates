36e8e1ee6acd77f26afd8b8e06fcb0f68e9bb9ba
Verhoevenwout <Wout@Wout-Verhoeven.local>
2017-06-26 22:13:11 +0100

    Commit

---
+++ developer/startProject/js/script.js
@@ -0,0 +1,15 @@
+//ANGULAR
+(function(){
+	var app = angular.module("mainApp",[]);
+    app.controller('MyNewController',['$scope','$http', function($scope, $http){
+
+    	//ANGULAR CODE
+    }]);
+})();
+
+//jQuery
+$(document).ready(function(){
+	console.log('ready');
+
+	//CODE
+});
